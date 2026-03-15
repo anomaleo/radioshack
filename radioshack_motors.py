e74b5b55c4cdcc07429078aa50c447462cb7300d
Elio Bidinost <62852884+anomaleo@users.noreply.github.com>
2026-03-15 11:17:05 -0400

    HTTP SERVER & Motor Code
    HTTP SERVER Events cleanup, motor code corrected.

--- radioshack_motors.py
+++ radioshack_motors.py
@@ -1,120 +1,84 @@
+# MOTOR DRIVER
 import board
 import time
-import math
-
-# MOTOR DRIVER
 import pwmio
-from digitalio import DigitalInOut, Direction, Pull
 from adafruit_motor import motor
 
-# DEFINE PWM CONTROL FOR MOTOR LEFT AND RIGHT. 
-PWM_PIN_A = board.SDA
-PWM_PIN_B = board.SCL
+PWM_PIN_A = board.SCK
+PWM_PIN_B = board.MISO
 pwm_a = pwmio.PWMOut(PWM_PIN_A, frequency=1600)
 pwm_b = pwmio.PWMOut(PWM_PIN_B, frequency=1600)
+# motor_left = motor.DCMotor(pwm_a, pwm_b)
 
-motor1 = motor.DCMotor(pwm_a, pwm_b)
+PWM_PIN_2A = board.TX
+PWM_PIN_2B = board.RX
+pwm_2a = pwmio.PWMOut(PWM_PIN_2A, frequency=1600)
+pwm_2b = pwmio.PWMOut(PWM_PIN_2B, frequency=1600)
+# motor_right = motor.DCMotor(pwm_2a, pwm_2b)
 
-PWM_PIN_2A = board.SCK
-PWM_PIN_2B = board.MISO
-pwm_2a = pwmio.PWMOut(PWM_PIN_2A, frequency=50)
-pwm_2b = pwmio.PWMOut(PWM_PIN_2B, frequency=50)
-motor2 = motor.DCMotor(pwm_2a, pwm_2b)
-# motor_l = motor.DCMotor(pwm_a, pwm_b)
+motors = [motor.DCMotor(pwm_a, pwm_b), motor.DCMotor(pwm_2a, pwm_2b)]
 
-motor_l_ctl_a = DigitalInOut(board.TX)
-motor_l_ctl_b = DigitalInOut(board.RX)
-motor_l_ctl_a.direction = Direction.OUTPUT
-motor_l_ctl_b.direction = Direction.OUTPUT
-# 
-motor2.throttle = 0.5
-# motor_l_ctl_a.value = True
-# motor_l_ctl_b.value = False
-# motor_r_ctl_a.value = True
-# motor_r_ctl_b.value = False
-# 
-time.sleep(2)
-# 
-motor2.throttle = -0.5
-# motor_l_ctl_a.value = False
-# motor_l_ctl_b.value = True
-# motor_r_ctl_a.value = True
-# motor_r_ctl_b.value = False
-# 
-time.sleep(2)
-# 
-motor2.throttle = 0.0
-# motor_l_ctl_a.value = True
-# motor_l_ctl_b.value = True
-# motor_r_ctl_a.value = True
-# motor_r_ctl_b.value = True
+def forward(motors, adv):
+	print("\nForwards")
+	motors[0].throttle = adv
+	motors[1].throttle = adv
+	time.sleep(0.001)
+	print("  throttle:", motors[0].throttle)
+	time.sleep(1)
+	
+def stop(motors, adv):
+	print("\nStop")
+	motors[0].throttle = adv
+	motors[1].throttle = adv
+	time.sleep(0.001)
+	print("  throttle:", motors[0].throttle)
+	time.sleep(1)
+	
+def backward(motors, adv):
+	print("\nBackwards")
+	motors[0].throttle = -adv
+	motors[1].throttle = -adv
+	time.sleep(0.001)
+	print("  throttle:", motors[0].throttle)
+	time.sleep(1)
+	
+def right(motors, adv):
+	print("\Right")
+	motors[0].throttle = adv
+	motors[1].throttle = -adv
+	time.sleep(0.001)
+	print("  throttle:", motors[0].throttle)
+	time.sleep(1)
+	
+def left(motors, adv):
+	print("\Left")
+	motors[0].throttle = -adv
+	motors[1].throttle = adv
+	time.sleep(0.001)
+	time.sleep(1)
 
 
-# while True:
-# 	pass
-# 	print("***DC motor test***")
-# 
-# 	print("\nForwards slow")
-# 	motor_l_ctl_a.value = True
-# 	motor_l_ctl_b.value = False
-# 	motor_l.throttle = 0.5
-# 	print("  throttle:", motor_l.throttle)
+# while True: 
+# 	forward(motors, 0.5)
 # 	time.sleep(1)
 # 
-# 	print("\nStop")
-# 	motor_l_ctl_a.value = True
-# 	motor_l_ctl_b.value = True
-# 	motor_l.throttle = 0
-# 	print("  throttle:", motor_l.throttle)
+# 	stop(motors, 0.0)
+# 	time.sleep(1)
+# 	
+# 	backward(motors, -0.5)
 # 	time.sleep(1)
 # 
-# 	print("\nForwards")
-# 	motor_l_ctl_a.value = True
-# 	motor_l_ctl_b.value = False
-# 	motor_l.throttle = 1.0
-# 	print("  throttle:", motor_l.throttle)
+# 	stop(motors, 0.0)
 # 	time.sleep(1)
-# 
-# 	print("\nStop")
-# 	motor_l_ctl_a.value = True
-# 	motor_l_ctl_b.value = True
-# 	motor_l.throttle = 0
-# 	print("throttle:", motor_l.throttle)
+# 	
+# 	left(motors, 0.5)
 # 	time.sleep(1)
-# 
-# 	print("\nBackwards")
-# 	motor_l_ctl_a.value = False
-# 	motor_l_ctl_b.value = True
-# 	motor_l.throttle = -1.0
-# 	print("  throttle:", motor_l.throttle)
+# 	
+# 	stop(motors, 0.0)
 # 	time.sleep(1)
-# 
-# 	print("\nStop")
-# 	motor_l_ctl_a.value = True
-# 	motor_l_ctl_b.value = True
-# 	motor_l.throttle = 0
-# 	print("throttle:", motor_l.throttle)
+# 	
+# 	right(motors, 0.5)
 # 	time.sleep(1)
-# 
-# 	print("\nBackwards slow")
-# 	motor_l_ctl_a.value = False
-# 	motor_l_ctl_b.value = True
-# 	motor_l.throttle = -0.5
-# 	print("  throttle:", motor_l.throttle)
+# 	
+# 	stop(motors, 0.0)
 # 	time.sleep(1)
-# 
-# 	print("\nStop")
-# 	motor_l_ctl_a.value = True
-# 	motor_l_ctl_b.value = True
-# 	motor_l.throttle = 0
-# 	print("  throttle:", motor_l.throttle)
-# 	time.sleep(1)
-# 
-# 	print("\nSpin freely")
-# 	motor_l_ctl_a.value = True
-# 	motor_l_ctl_b.value = False
-# 	motor_l.throttle = None
-# 	print("  throttle:", motor_l.throttle)
-# 
-# 	print("\n***Motor test is complete***")
-
